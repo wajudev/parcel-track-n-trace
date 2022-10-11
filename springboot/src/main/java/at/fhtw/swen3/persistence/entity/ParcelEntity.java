b8298bc862f67b193ec324fbf3075f23e8631832
TomMihnjak <if20b277@technikum-wien.at>
2022-10-11 20:30:18 +0200

    ParcelEntity.java validation added

--- springboot/src/main/java/at/fhtw/swen3/persistence/entity/ParcelEntity.java
+++ springboot/src/main/java/at/fhtw/swen3/persistence/entity/ParcelEntity.java
@@ -7,6 +7,9 @@ import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.Setter;
 
+import javax.validation.constraints.Min;
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Pattern;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -14,11 +17,19 @@ import java.util.List;
 @Setter
 @AllArgsConstructor
 public class ParcelEntity {
+    @Min(value = 0, message = "Gewicht kann nicht < 0 sein")
     private Float weight;
+    @NotNull
     private Recipient recipient;
+    @NotNull
     private Recipient sender;
     private TrackingInformation.StateEnum state;
+    @NotNull
+    @Pattern(regexp = "^[A-Z]{4}\\d{1,4}$", message = "Falsches FutureHopPattern Pattern")
     private List<HopArrival> futureHops = new ArrayList<>();
+    @NotNull
+    @Pattern(regexp = "^[A-Z]{4}\\d{1,4}$", message = "Falsches trackingID Pattern")
     private List<HopArrival> visitedHops = new ArrayList<>();
+    @Pattern(regexp = "^[A-Z0-9]{9}$", message = "Falsches trackingID Pattern")
     private String trackingId;
 }
