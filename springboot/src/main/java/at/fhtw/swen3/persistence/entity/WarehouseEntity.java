503ad62fac449c15edee5162fb77ef9575799993
Waju <lanre.ajibua@gmail.com>
2022-10-29 21:14:17 +0200

    Field hop of class WarehouseEntity

--- springboot/src/main/java/at/fhtw/swen3/persistence/entity/WarehouseEntity.java
+++ springboot/src/main/java/at/fhtw/swen3/persistence/entity/WarehouseEntity.java
@@ -5,6 +5,8 @@ import lombok.Getter;
 import lombok.Setter;
 
 import javax.persistence.Entity;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
 import javax.persistence.OneToMany;
 import javax.validation.constraints.NotNull;
 import java.util.ArrayList;
@@ -20,4 +22,5 @@ public class WarehouseEntity extends HopEntity{
     @NotNull
     private List<WarehouseNextHopsEntity> nextHops = new ArrayList<>();
 
+    private String hop;
 }
