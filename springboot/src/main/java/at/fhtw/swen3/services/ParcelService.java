7bb43d757a5f99cab072221da62871e1af9e0183
Tom Mihnjak <if20b277@technikum-wien.at>
2022-11-12 11:28:50 +0100

    sprint 4 created

--- springboot/src/main/java/at/fhtw/swen3/services/ParcelService.java
+++ springboot/src/main/java/at/fhtw/swen3/services/ParcelService.java
@@ -2,9 +2,13 @@ package at.fhtw.swen3.services;
 
 import at.fhtw.swen3.persistence.entities.ParcelEntity;
 import at.fhtw.swen3.persistence.repositories.ParcelRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.stereotype.Service;
 
 import java.util.List;
-
+@Service
+@Configuration
 public interface ParcelService {
 
     List<ParcelEntity> getAllParcel();
