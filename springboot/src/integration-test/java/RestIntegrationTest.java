c31117ee14ee8a4d9f30fafe3cae5a1b5c930649
Waju <lanre.ajibua@gmail.com>
2023-01-15 16:01:47 +0100

    integration-test aufbau

---
+++ springboot/src/integration-test/java/RestIntegrationTest.java
@@ -0,0 +1,34 @@
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.http.MediaType;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.util.ResourceUtils;
+
+import java.io.File;
+import java.nio.file.Files;
+
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+@AutoConfigureMockMvc
+public class RestIntegrationTest {
+
+    private final MockMvc mockMvc;
+
+    public RestIntegrationTest(MockMvc mockMvc) {
+        this.mockMvc = mockMvc;
+    }
+
+    @Test
+    public void post_warehouse() throws Exception {
+        // Read the JSON file
+        File file = ResourceUtils.getFile("classpath:test.json");
+        String json = new String(Files.readAllBytes(file.toPath()));
+
+        // Send the POST request and verify the response
+        mockMvc.perform(post("/warehouse")
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .content(json))
+                .andExpect(status().isOk());
+    }
+}
